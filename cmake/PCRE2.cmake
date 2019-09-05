8905fc6954edd7b5687fa95bb13b79671a6c3823
Shahriar <shahriar25.ss@gmail.com>
2019-09-05 12:07:58 +0430

    Fixed pcre2 cmake

--- cmake/PCRE2.cmake
+++ cmake/PCRE2.cmake
@@ -1,9 +1,8 @@
 set(PCRE2_BUILD_PCRE2_8 OFF CACHE BOOL "Build 8bit PCRE2 library")
 set(PCRE2_BUILD_PCRE2_32 ON CACHE BOOL "Build 32bit PCRE2 library")
 
-find_library(PCRE2_LIB pcre2-32)
-find_path(PCRE2_INCLUDE_DIR pcre2.h)
 add_subdirectory(3rd/pcre2 EXCLUDE_FROM_ALL)
-
+set(PCRE2_INCLUDE_DIR ${CMAKE_BINARY_DIR}/pcre2/)
 set(PCRE2_LIB pcre2-32)
+
 include_directories(${PCRE2_INCLUDE_DIR})
